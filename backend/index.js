a3e8c9f15130e71779b965407e41adf64c9338be
taffy <ececanous@gmail.com>
2025-03-08 18:29:23 +1100

    Created TopSocial project

---
+++ backend/index.js
@@ -0,0 +1,42 @@
+// 这里主要创建server
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const express = require('express')
+const router = express.Router()
+const cors = require ('cors')
+
+
